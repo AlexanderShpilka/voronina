3e089f7612133af5618cdae36d20fc437852b36e
AlexanderShpilka <alexander.shpilka@gmail.com>
2020-07-07 15:01:28 +0300

    Close mobile menu after link click

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,6 +1,7 @@
 const openMenuBtn = document.querySelector('.js-open-menu');
 const closeMenuBtn = document.querySelector('.js-close-menu');
 const navigation = document.querySelector('.js-nav');
+const navLinks = document.querySelectorAll('.js-nav-link');
 
 function toggleMenuVisibility() {
   navigation.classList.toggle('nav--visible');
@@ -8,3 +9,9 @@ function toggleMenuVisibility() {
 
 openMenuBtn.addEventListener('click', toggleMenuVisibility);
 closeMenuBtn.addEventListener('click', toggleMenuVisibility);
+
+navLinks.forEach(function (link) {
+  link.addEventListener('click', function () {
+    navigation.classList.remove('nav--visible');
+  });
+});
