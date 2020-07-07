f745ddc3440e6a7d58a447d8c64e6d370be51f4b
AlexanderShpilka <alexander.shpilka@gmail.com>
2020-07-07 11:31:56 +0300

    Rename and refactor script

--- src/js/menu.js
+++ src/js/menu.js
@@ -2,7 +2,9 @@ const openMenuBtn = document.querySelector('.js-open-menu');
 const closeMenuBtn = document.querySelector('.js-close-menu');
 const navigation = document.querySelector('.js-nav');
 
-const toggleMenuVisibility = () => navigation.classList.toggle('nav--visible');
+function toggleMenuVisibility() {
+  navigation.classList.toggle('nav--visible');
+}
 
 openMenuBtn.addEventListener('click', toggleMenuVisibility);
 closeMenuBtn.addEventListener('click', toggleMenuVisibility);
